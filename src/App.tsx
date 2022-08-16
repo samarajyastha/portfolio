9251fc40b575cd67b217741d090ed3e3b82bb925
samarajya <samarajya@dzangolab.com>
2022-08-16 10:23:43 +0545

    feat: add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { declaration } from "./constants/contents";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+import "./App.css";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <Header />
+      <Footer declaration={declaration} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/Footer.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+import * as palette from "../constants/colors";
+
+export const StyledFooter = styled.footer`
+  color: ${palette.primaryTextColor};
+  margin: 1rem 0;
+  text-align: justify;
+`;
