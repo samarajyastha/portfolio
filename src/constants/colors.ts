1fc6d209e813420b7ab3d07ba149c2c0187f9adf
samarajya <samarajya@dzangolab.com>
2022-08-16 10:23:29 +0545

    feat: add constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,6 @@
+export const black = "#000000";
+export const pills = "#00c8aa";
+export const primaryBgColor = "#e1edfb";
+export const primaryColor = "#007aff";
+export const primaryTextColor = "#333232";
+export const white = "#ffffff";
---
+++ src/constants/contents.ts
@@ -0,0 +1,2 @@
+export const declaration =
+  "I hereby declare that all the information provided above is true and genuine to the best of my knowledge. I will solely be responsible for any discrepancy found in them.";
